aa6140bae0b9a3dd1d0717ce1dad336349272358
Krishna Murthi <krishnamurthikm07@gmail.com>
2025-11-27 01:36:16 +0530

    Update frontend API URL to Render backend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://uttaranewebsite-v2-2.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
